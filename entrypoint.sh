720f51d2d01c442723ee39ef9f3b21af65efb970
David Perov <david.perov60@gmail.com>
2025-09-04 22:01:02 +0300

    fix: not starting init neo4j scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Running Neo4j initialization..."
-
-node dist/cli.js init-neo4j
+# echo "Running Neo4j initialization..."
+# node dist/cli.js init-neo4j
 
 echo "Starting application..."
 
